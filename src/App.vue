120bd07d8a2dabdcba79707d4eb8f78a5b27f13c
Lukas Forst <lukas@forst.pw>
2020-03-15 12:32:43 +0100

    fuck that, I hate css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/me.png">
+    <h4><a href="https://github.com/LukasForst" target="_blank">Lukas Forst</a> </h4>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
