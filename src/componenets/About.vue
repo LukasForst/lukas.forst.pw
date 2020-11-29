2a8df08e391cd2dc2950c9e5a4e6eaabcd8bd792
Lukas Forst <lukas@forst.pw>
2020-11-29 21:19:04 +0100

    add social and update about

--- src/componenets/About.vue
+++ src/componenets/About.vue
@@ -1,42 +1,71 @@
 <template>
-    <div>
-        <img class="photo-me" alt="My photo." src="../assets/me.png">
+  <div>
+    <img class="photo-me" alt="My photo." src="../assets/me.png">
 
-        <h1>Lukas Forst</h1>
+    <h1>Lukas Forst</h1>
 
-        <md-button class="md-fab md-mini md-icon-button" :href="github" target="_blank">
-            <md-icon class="fa fa-github"/>
-        </md-button>
+    <div class="social-container">
+      <md-button class="md-fab md-mini md-icon-button" :href="linkedin" target="_blank">
+        <md-icon class="fa fa-linkedin"/>
+        <md-tooltip md-direction="bottom">
+          <div class="tooltip-text">LinkedIn</div>
+        </md-tooltip>
+      </md-button>
 
-        <md-button class="md-fab md-mini md-icon-button" :href="linkedin" target="_blank">
-            <md-icon class="fa fa-linkedin"/>
-        </md-button>
+      <md-button class="md-fab md-mini md-icon-button" :href="cv" target="_blank">
+        <md-icon class="fa fa-user"/>
+        <md-tooltip md-direction="bottom">
+          <div class="tooltip-text">CV</div>
+        </md-tooltip>
+      </md-button>
 
-        <md-button class="md-fab md-mini md-icon-button" :href="mail">
-            <md-icon class="fa fa-envelope-o"/>
-        </md-button>
+      <md-button class="md-fab md-mini md-icon-button" :href="github" target="_blank">
+        <md-icon class="fa fa-github"/>
+        <md-tooltip md-direction="bottom">
+          <div class="tooltip-text">Github</div>
+        </md-tooltip>
+      </md-button>
+
+      <md-button class="md-fab md-mini md-icon-button" :href="mail">
+        <md-icon class="fa fa-envelope-o"/>
+        <md-tooltip md-direction="bottom">
+          <div class="tooltip-text">Mail</div>
+        </md-tooltip>
+      </md-button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "About",
-        data() {
-            return {
-                linkedin: 'https://www.linkedin.com/in/lukas-forst/',
-                github: 'https://github.com/LukasForst',
-                mail: 'mailto:lukas@forst.pw'
-            }
-        }
+export default {
+  name: "About",
+  data() {
+    return {
+      linkedin: 'https://www.linkedin.com/in/lukas-forst/',
+      github: 'https://github.com/LukasForst',
+      mail: 'mailto:lukas@forst.pw',
+      cv: 'https://cv.forst.pw'
     }
+  }
+}
 </script>
 
 <style scoped>
-    a {
-        color: inherit;
-    }
+a {
+  color: inherit;
+}
 
-    .photo-me {
-        border-radius: 50%;
-    }
+.photo-me {
+  border-radius: 50%;
+}
+
+.social-container {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+}
+
+.tooltip-text {
+  font-size: 1rem;
+}
 </style>
